6412e4d286a7b96f644b2d96ed20696775854c3d
Marijjo4 <139260649+Marijjo4@users.noreply.github.com>
2023-07-12 12:06:30 -0600

    Add files via upload

---
+++ 1-notebook.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1> Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. SQL
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# 1. Pandas
+# 2. Streamlit
+# 3. ggplot2
+# 4. dplyr
+
+# |Data Science Tools
+# -----------------
+# |GIT
+# |GITHUB
+# |Azure Synapse
+
+# <h3> Below are a few examples of evaluating arithmetic expressions in Python
+
+# |OPERADOR | FUNCIÓN | EJEMPLO
+# ----|------|---
+# |- |RESTA|12 - 3 = 9
+# |* |MULTIPLICACION|	12 * 3 = 36
+# |/ |DIVISION	|12 / 3 = 4
+# |% |MÓDULO|	16 % 3 = 1
+
+# This a simple arithmetic expression to mutiply then add integers
+
+# In[6]:
+
+
+(3*4)+5
+
+
+# This will convert 200 minutes to hours by diving by 60
+
+# In[7]:
+
+
+200/60
+
+
+# **Objectives:**
+
+# * Conocer los lenguajes más populares para la Ciencia de Datos
+# * Ubicar las expresiones básicas aritméticas en Python
+# * Conocer las principales librerias que se utilizan para la Ciencia de Datos
+
+# <h2> Author
+
+# María José Bolaños
+
+# In[ ]:
+
+
+
+
